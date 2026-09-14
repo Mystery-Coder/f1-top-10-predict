9fef19ca2aa68635127e221efb52b6514507f84c
MysteryCoder <srikar0811@gmail.com>
2026-09-14 18:00:39 +0530

    feat: new favicon

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.0.171", "192.168.*.*", "10.0.*.*"],
 };
 
 export default nextConfig;
